8a3275eff5fd0c707c96b7d73bcabf98d8355f8d
Javier <jaasmu.9@gmail.com>
2026-03-28 20:43:42 -0500

    Update detect_distro.sh

--- modules/core/detect_distro.sh
+++ modules/core/detect_distro.sh
@@ -2,11 +2,10 @@
 
 detect_distro(){
     if [ -f /etc/os-release ]; then
-    . /etc/os-release
-    DISTRO=$ID
+        . /etc/os-release
+        DISTRO=$ID
     else
-
-    echo '[!] Cannot detect linux distribution.'
-    exit 1
+        echo '[!] Cannot detect linux distribution.'
+        exit 1
     fi
 }
